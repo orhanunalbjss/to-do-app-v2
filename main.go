84abca9acf69a585232708dad7d8aa56b125b52d
Orhan Unal <orhan.unal@bjss.com>
2025-01-28 22:52:32 +0000

    Save to-do items to a file on disk

--- main.go
+++ main.go
@@ -1,27 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
 type Item struct {
-	name        string
-	description string
-	status      string
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
 }
 
-var items []Item
+var Items []Item
 
 func main() {
 	nameFlag := flag.String("name", "Buy a new backpack", "item name")
-	descriptionFlag := flag.String("description", "item description", "item description")
-	statusFlag := flag.String("status", "not started", "item status")
+	descriptionFlag := flag.String("description", "Prefer red or blue", "item description")
+	statusFlag := flag.String("status", "Not Started", "item status")
 
 	flag.Parse()
 
-	item := Item{name: *nameFlag, description: *descriptionFlag, status: *statusFlag}
-	items = append(items, item)
+	item := Item{Name: *nameFlag, Description: *descriptionFlag, Status: *statusFlag}
+	Items = append(Items, item)
 
-	fmt.Println(items)
+	fmt.Println(Items)
+
+	file, err := os.Create("items.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(file *os.File) {
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(Items); err != nil {
+		log.Fatal(err)
+	}
 }
