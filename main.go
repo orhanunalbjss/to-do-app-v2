698e7672bdad23815fd33877ebcede218de67853
Orhan Unal <orhan.unal@bjss.com>
2025-01-29 00:40:05 +0000

    Refactor to clean up main function

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"os"
 )
 
+const ItemsFilename = "items.json"
+
 type Item struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
@@ -17,7 +19,24 @@ type Item struct {
 var Items []Item
 
 func main() {
-	file, err := os.Open("items.json")
+	loadItemsFromDisk()
+
+	nameFlag := flag.String("name", "Buy a new backpack", "item name")
+	descriptionFlag := flag.String("description", "Prefer red or blue", "item description")
+	statusFlag := flag.String("status", "Not Started", "item status")
+
+	flag.Parse()
+
+	item := Item{Name: *nameFlag, Description: *descriptionFlag, Status: *statusFlag}
+	Items = append(Items, item)
+
+	fmt.Println(Items)
+
+	saveItemsToDisk()
+}
+
+func loadItemsFromDisk() {
+	file, err := os.Open(ItemsFilename)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,22 +51,19 @@ func main() {
 	if err := decoder.Decode(&Items); err != nil {
 		log.Fatal(err)
 	}
+}
 
-	nameFlag := flag.String("name", "Buy a new backpack", "item name")
-	descriptionFlag := flag.String("description", "Prefer red or blue", "item description")
-	statusFlag := flag.String("status", "Not Started", "item status")
-
-	flag.Parse()
-
-	item := Item{Name: *nameFlag, Description: *descriptionFlag, Status: *statusFlag}
-	Items = append(Items, item)
-
-	fmt.Println(Items)
-
-	file, err = os.Create("items.json")
+func saveItemsToDisk() {
+	file, err := os.Create(ItemsFilename)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	defer func(file *os.File) {
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}(file)
 
 	encoder := json.NewEncoder(file)
 	if err := encoder.Encode(Items); err != nil {
