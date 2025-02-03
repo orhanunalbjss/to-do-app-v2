50cdb618e71ea6bf22cdc1e1a5b8c503526f2a03
Orhan Unal <orhan.unal@bjss.com>
2025-02-03 20:29:13 +0000

    refactor to separate core todo store logic into a different package to main/cli code

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,25 +2,13 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
-	"fmt"
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
-	"io/fs"
 	"log/slog"
 	"os"
+	js "to-do-app-v2/pkg/jsonstore"
 )
-
-type Item struct {
-	Name   string `json:"name"`
-	Desc   string `json:"description"`
-	Status string `json:"status"`
-}
-
-func (item Item) String() string {
-	return fmt.Sprintf("Name: %s, Description: %s, Status: %s", item.Name, item.Desc, item.Status)
-}
 
 type ContextHandler struct {
 	slog.Handler
@@ -33,10 +21,6 @@ func (handler *ContextHandler) Handle(context context.Context, record slog.Recor
 	return handler.Handler.Handle(context, record)
 }
 
-const ItemsFilename = "items.json"
-
-var Items []Item
-
 func main() {
 	var err error
 	ctx := context.WithValue(context.Background(), "TraceID", uuid.NewString())
@@ -47,7 +31,7 @@ func main() {
 	args := flag.Args()
 	if len(args) == 0 {
 		slog.ErrorContext(ctx, "expected one of the following commands: add, list, update, delete")
-		os.Exit(1)
+		return
 	}
 
 	cmd, cmdArgs := args[0], args[1:]
@@ -84,7 +68,7 @@ func main() {
 		slog.InfoContext(ctx, "item deleted")
 	default:
 		slog.ErrorContext(ctx, "expected one of the following commands: add, list, update, delete")
-		os.Exit(1)
+		return
 	}
 
 	// Keep app running until SIGINT (CTRL+C) signal is sent
@@ -121,15 +105,15 @@ func addCommand(args []string) error {
 		return errors.Wrapf(err, "parse arguments: %v", args)
 	}
 
-	if err = loadItems(); err != nil {
+	if err = js.LoadItems(); err != nil {
 		return errors.Wrap(err, "load items")
 	}
 
-	item := Item{Name: name, Desc: desc, Status: status}
+	item := js.Item{Name: name, Desc: desc, Status: status}
 
-	addItem(item)
-	listItems()
-	if err = saveItems(); err != nil {
+	js.AddItem(item)
+	js.ListItems()
+	if err = js.SaveItems(); err != nil {
 		return errors.Wrap(err, "save items")
 	}
 
@@ -139,11 +123,11 @@ func addCommand(args []string) error {
 func listCommand() error {
 	var err error
 
-	if err = loadItems(); err != nil {
+	if err = js.LoadItems(); err != nil {
 		return errors.Wrap(err, "load items")
 	}
 
-	listItems()
+	js.ListItems()
 
 	return nil
 }
@@ -166,17 +150,17 @@ func updateCommand(args []string) error {
 		return errors.Wrapf(err, "parse arguments: %v", args)
 	}
 
-	if err = loadItems(); err != nil {
+	if err = js.LoadItems(); err != nil {
 		return errors.Wrap(err, "load items")
 	}
 
-	item := Item{Name: name, Desc: desc, Status: status}
+	item := js.Item{Name: name, Desc: desc, Status: status}
 
-	if err = updateItem(id, item); err != nil {
+	if err = js.UpdateItem(id, item); err != nil {
 		return errors.Wrap(err, "update item")
 	}
-	listItems()
-	err = saveItems()
+	js.ListItems()
+	err = js.SaveItems()
 
 	return errors.Wrap(err, "save items")
 }
@@ -193,100 +177,15 @@ func deleteCommand(args []string) error {
 		return errors.Wrapf(err, "parse arguments: %v", args)
 	}
 
-	if err = loadItems(); err != nil {
+	if err = js.LoadItems(); err != nil {
 		return errors.Wrap(err, "load items")
 	}
 
-	if err = deleteItem(id); err != nil {
+	if err = js.DeleteItem(id); err != nil {
 		return errors.Wrap(err, "delete item")
 	}
-	listItems()
-	err = saveItems()
+	js.ListItems()
+	err = js.SaveItems()
 
 	return errors.Wrap(err, "save items")
-}
-
-func addItem(item Item) {
-	Items = append(Items, item)
-}
-
-func listItems() {
-	for index, item := range Items {
-		fmt.Printf("%d: %s\n", index+1, item)
-	}
-}
-
-func updateItem(id int, item Item) error {
-	if !isValidId(id) {
-		return fmt.Errorf("invalid id: %d", id)
-	}
-
-	Items[id-1].Name = item.Name
-	Items[id-1].Desc = item.Desc
-	Items[id-1].Status = item.Status
-
-	return nil
-}
-
-func deleteItem(id int) error {
-	if !isValidId(id) {
-		return fmt.Errorf("invalid id: %d", id)
-	}
-
-	Items = append(Items[:id-1], Items[id:]...)
-
-	return nil
-}
-
-func isValidId(id int) bool {
-	return id >= 1 && id <= len(Items)
-}
-
-func loadItems() (err error) {
-	var file *os.File
-	file, err = os.Open(ItemsFilename)
-	if err != nil {
-		if errors.Is(err, fs.ErrNotExist) {
-			Items = make([]Item, 0)
-			if err = saveItems(); err != nil {
-				return errors.Wrap(err, "save items")
-			}
-
-			return
-		}
-
-		return errors.Wrapf(err, "open %s", ItemsFilename)
-	}
-
-	defer func() {
-		closeError := file.Close()
-		if err == nil {
-			err = closeError
-		}
-	}()
-
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&Items)
-
-	return errors.Wrapf(err, "decode %s", ItemsFilename)
-}
-
-func saveItems() (err error) {
-	var file *os.File
-	file, err = os.Create(ItemsFilename)
-	if err != nil {
-		return errors.Wrapf(err, "create %s", ItemsFilename)
-	}
-
-	defer func() {
-		closeError := file.Close()
-		if err == nil {
-			err = errors.Wrapf(closeError, "close %s", ItemsFilename)
-		}
-	}()
-
-	encoder := json.NewEncoder(file)
-	err = encoder.Encode(Items)
-
-	return errors.Wrapf(err, "encode %s", ItemsFilename)
 }
